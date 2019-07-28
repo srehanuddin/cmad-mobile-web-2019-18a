c459c3926ab8840c2e0e119e251086f4514c8fdf
Muhammad Tabish <tabish615@gmail.com>
2019-07-28 21:40:19 +0500

    class 2019-07-28

---
+++ React Native/First_App_React_Native/App.js
@@ -0,0 +1,170 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, {Fragment} from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+  TextInput,
+  Button
+} from 'react-native';
+
+import {
+  Header,
+  LearnMoreLinks,
+  Colors,
+  DebugInstructions,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+
+
+class App extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      value : "",
+      allValues : []
+    }
+    this.add = this.add.bind(this)
+    this.dataSave = this.dataSave.bind(this)
+    this.delData = this.delData.bind(this)
+
+  }
+
+  add(e){
+    // console.warn(e)
+    this.setState({
+      value : e
+    })
+  }
+  dataSave(){
+    this.setState({
+      allValues : [...this.state.allValues, this.state.value]
+    })
+  }
+
+  delData(element){
+    this.setState({
+      allValues : this.state.allValues.splice(element, 1)
+    })
+  }
+
+  render(){
+  return (
+    // <Fragment>
+    //   <StatusBar barStyle="dark-content" />
+    //   <SafeAreaView>
+    //     <ScrollView
+    //       contentInsetAdjustmentBehavior="automatic"
+    //       style={styles.scrollView}>
+    //       <Header />
+    //       {global.HermesInternal == null ? null : (
+    //         <View style={styles.engine}>
+    //           <Text style={styles.footer}>Engine: Hermes</Text>
+    //         </View>
+    //       )}
+    //       <View style={styles.body}>
+    //         <View style={styles.sectionContainer}>
+    //           <Text style={styles.sectionTitle}>Step One</Text>
+    //           <Text style={styles.sectionDescription}>
+    //             Edit <Text style={styles.highlight}>App.js</Text> to change this
+    //             screen and then come back to see your edits.
+    //           </Text>
+    //         </View>
+    //         <View style={styles.sectionContainer}>
+    //           <Text style={styles.sectionTitle}>See Your Changes</Text>
+    //           <Text style={styles.sectionDescription}>
+    //             <ReloadInstructions />
+    //           </Text>
+    //         </View>
+    //         <View style={styles.sectionContainer}>
+    //           <Text style={styles.sectionTitle}>Debug</Text>
+    //           <Text style={styles.sectionDescription}>
+    //             <DebugInstructions />
+    //           </Text>
+    //         </View>
+    //         <View style={styles.sectionContainer}>
+    //           <Text style={styles.sectionTitle}>Learn More</Text>
+    //           <Text style={styles.sectionDescription}>
+    //             Read the docs to discover what to do next:
+    //           </Text>
+    //         </View>
+    //         <LearnMoreLinks />
+    //       </View>
+    //     </ScrollView>
+    //   </SafeAreaView>
+    // </Fragment>
+    <View>
+      <Text>Todo App</Text>
+      <TextInput
+        placeholder="Enter Data..."
+        onChangeText={(text) => this.add(text)}
+      />
+      <Button
+        title = "Add"
+        onPress = {()=> this.dataSave()}
+      />
+      {this.state.allValues.map((data, index) => {
+        return <View key={index}>
+        <Text>{data}</Text>
+        <Button
+          title = "Delete"
+          onPress = {()=> this.delData(index)}
+        />
+        </View>
+      })}
+    </View>
+  );
+  }
+};
+
+const styles = StyleSheet.create({
+  scrollView: {
+    backgroundColor: Colors.lighter,
+  },
+  engine: {
+    position: 'absolute',
+    right: 0,
+  },
+  body: {
+    backgroundColor: Colors.white,
+  },
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+    color: Colors.black,
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+    color: Colors.dark,
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+  footer: {
+    color: Colors.dark,
+    fontSize: 12,
+    fontWeight: '600',
+    padding: 4,
+    paddingRight: 12,
+    textAlign: 'right',
+  },
+});
+
+export default App;
